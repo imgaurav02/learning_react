71ef48e6fb5d1d12574bab2d6c0e70d71bdf0211
Gaurav <imgaurave02@gmail.com>
2024-01-09 21:45:38 +0530

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,26 @@
+import { useContext, useState } from "react"
+import UserContext from "../context/UserContext"
+
+export default function login (){
+    const [username,setUsername] = useState("")
+    const [password,setPassword] = useState("")
+
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser({username,password})
+    }
+    return (
+        <>
+            <div>
+                <h1>Login</h1>
+                <input type="text"  value = {username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+                {" "}
+                <input type="password" value = {password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+
+                <button onClick={handleSubmit}>Submit</button>
+            </div>
+        </>
+    )
+}
