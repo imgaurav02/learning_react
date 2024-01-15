bd623351977e5d4de5c929a67b475c93f5919138
Gaurav <imgaurave02@gmail.com>
2024-01-15 23:49:59 +0530

    added login signup authLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import {useDispatch} from 'react-redux';
 import conf from './conf/conf';
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  console.log(conf.appwriteUrl);
-  return (
-    <>
-      <h1>Gaurav's Blog</h1>
-    </>
-  )
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => {setLoading(false)})
+  },)
+  return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+  </div>) : (
+    <div>
+      <h1>loading....</h1>
+    </div>
+  ) 
 }
 
 export default App
